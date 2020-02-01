ad68ad44cec9c247b75a814aa9122ae65935df1f
Shunfu Mao <shunfu@uw.edu>
2020-01-31 16:09:51 -0800

    add trinity related software

--- RefShannon/dep_path.py
+++ RefShannon/dep_path.py
@@ -7,6 +7,8 @@ extAssembler_paths = {
   "ryuto": "/home/shunfu/software/ryuto28/ryuto",
   "strawberry" : "/home/shunfu/software/strawberry1.0.2/strawberry",
   "scallop": "/home/shunfu/software/scallop-0.10.2_linux_x86_64/scallop",
+  # need java 1.8, 
+  "trinity": "Trinity", # export in bashrc: /home/shunfu/software/trinity/trinityrnaseq-v2.9.1/
 }
 
 """
@@ -21,4 +23,8 @@ tool_paths = {
   "blat": "/home/shunfu/software/blat/blat/blat",
   "gffread": "/home/shunfu/software/gffread-0.9.12.Linux_x86_64/gffread",
   "gffcompare": "/home/shunfu/software/gffcompare-0.10.4.Linux_x86_64/gffcompare",
+  # trinity depends on bowtie2, jellyfish and salmon
+  "bowtie2": "bowtie2", # export in bashrc: /home/shunfu/software/bowtie2/bowtie2-2.3.5.1-linux-x86_64/
+  "jellyfish": "jellyfish", # export in bashrc: /home/shunfu/software/jellyfish/bin/
+  "salmon": "salmon", # export in bashrc: /home/shunfu/software/salmon/salmon-latest_linux_x86_64/bin/
 }
